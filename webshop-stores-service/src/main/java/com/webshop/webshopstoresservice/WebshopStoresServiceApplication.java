3210dbef2b7af9f1d818df90f824397953e609fb
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-06-24 14:15:18 +0200

    Turn inventory service and stores service into Eureka clients

--- webshop-stores-service/src/main/java/com/webshop/webshopstoresservice/WebshopStoresServiceApplication.java
+++ webshop-stores-service/src/main/java/com/webshop/webshopstoresservice/WebshopStoresServiceApplication.java
@@ -2,8 +2,10 @@ package com.webshop.webshopstoresservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class WebshopStoresServiceApplication {
 
 	public static void main(String[] args) {
