49f9c9e91dbf83ba9fff01be727392ad2779b623
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-06-24 17:31:42 +0200

    Put SQL timezone in connection string

--- webshop-inventory-service/src/main/java/com/webshop/webshopinventoryservice/resources/ProductResource.java
+++ webshop-inventory-service/src/main/java/com/webshop/webshopinventoryservice/resources/ProductResource.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
